f8eeff8fdfda9dcbb56026a5d408be34d38828fb
LinearLiu <17817363770@163.com>
2017-11-07 14:29:06 +0800

    2017年11月7日14:28:38-Create mini web package.

---
+++ test_303_mini_web_pack.js
@@ -0,0 +1,30 @@
+var http = require("http");
+var url = require("url");
+var fs = require("fs");
+var path = require('path');
+
+http.createServer(function(req,res){
+    //获取用户请求的路径，req.url并处理
+    var pathname = url.parse(req.url).pathname;
+    
+    //默认的首页
+    if(pathname == "/"){
+        pathname = "index.html";
+    }
+
+    //获取扩展名
+    var extname = path.extname(pathname);
+
+    fs.readFile("./static/" + pathname,function(err,data){
+        if(err){
+            fs.readFile("./static/404.html",function(err,data){
+                res.writeHead(404,{"Content-type":"text/html;charset=UTF8"});
+                res.end(data);
+            })
+            return;
+        }
+
+        res.writeHead(200,{"Content-type":"text/html"});
+        res.end(data);
+    })
+}).listen(3000,"127.0.0.1");
