a38e401f18ac75d270a30261937b92ed17bf3e81
LinearLaw <m18720991459_1@163.com>
2018-01-03 21:28:18 +0800

    Add module mongoose

--- README.md
+++ README.md
@@ -3,6 +3,9 @@
 
 ## 启动数据库
 
+        双击 mongodb_start.bat，即可开启数据库，
+        运行应用时，应保持数据库的链接，黑框不要关闭。
+        
         打开命令行
             mongod --dbpath ~\MongoData     启动数据库
             mongo       进入mongo环境，可以对数据库进行操作
---
+++ test_308_mongoose/model/db.js
@@ -0,0 +1,22 @@
+/**
+ * @desc mongoose提供了一整套的访问数据库的方式，
+ *      和数据库的连接、增删操作已经集成到了mongoose中
+ */
+
+const settings = require("../../test_307_express/settings.js")
+const mongoose = require("mongoose");
+
+//链接数据库
+mongoose.connect(settings.dburl);
+
+mongoose.connection.on("connected",()=>{
+    console.log("DB数据库已经连接: " + settings.dburl);
+})
+
+mongoose.connection.on("error",(err)=>{
+    console.log("DB数据库连接失败: " + err);
+})
+
+mongoose.connection.on("disconnected",()=>{
+    console.log("数据库断开连接。")
+})
