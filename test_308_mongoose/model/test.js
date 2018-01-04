75398172992ea991f662d2c03b7ad19d95128a81
LinearLaw <m18720991459_1@163.com>
2018-01-04 23:20:44 +0800

    Complete mongoose

--- test_308_mongoose/model/test.js
+++ test_308_mongoose/model/test.js
@@ -36,7 +36,7 @@ let initUser = {
      * 3、find by id and update，和update类似，但是它以id作为查询条件
      */
     updateById(){
-        let id = "id";
+        let id = "_id";
         let updateJSON = {"username":"xxx"};
         User.findByIdAndUpdate(id,updateJSON,(err,res)=>{
             if(err){console.log("Error: " + err)}
@@ -48,7 +48,90 @@ let initUser = {
     /**
      * 4、删除
      */
+    removeSth(){
+        let findObj = {"username":"admin"};
+        User.remove(findObj,(err,res)=>{
+            if(err){
+                console.log(err);
+            }else{
+                console.log('Res: ' + res);
+                //res => {"ok":1,"n":1}
+            }
+        })
+    },
+    /**
+     * 5、查询
+     *      范围查询：findObj = { "age":{ $gte:21,$lte:30 } } 从21到30的区间
+     */
+    findByConditions(findObj1,findObj2){
+        let findObj = {"username":"admin"};
+        if(!findObj2){
+            //纯粹查询，只需要输入条件，输出所有查询到的内容
+            User.find(findObj,(err,res)=>{
+                if(err){
+                    console.log('err');
+                }else{
+                    console.log('Res: ' + res);
+                }
+            })
+        }else{
+            //限制查询，指定查询到的内容，1表示输出该内容，0表示不输出
+            let findObjLimit = {"username":1,"_id":0}
+            User.find(findObj,findObjLimit,(err,res)=>{
 
+            })
+        }
+
+    },
+    /**
+     * 6、数量查询
+     */
+    findCount(){
+        let findObj = {};
+        User.count(findObj,(err,res)=>{
+            if(err){
+                console.log('Err: ' + err);
+            }else{
+                console.log('res: ' + res);
+            }
+        })
+    },
+    /**
+     * 7、根据id查询
+     */
+    findById(){
+        let findId = "_id";
+        User.findById(findId,(err,res)=>{
+            if(err){
+                console.log('err: ' + err);
+            }else{
+                console.log('res',res);
+            }
+        })
+    },
+    /**
+     * 8、模糊查询
+     */
+    findByRegx(){
+        let findReg = {"username":{$regex:/m/i}};//所有username里有m的都会被查询
+        User.find(findReg,(err,res)=>{
+            err?console.log('err',err):console.log('res',res);
+        })
+    },
+    /**
+     * 9、按页码查询
+     */
+    findByPage(){
+        let pageSize = 5;
+        let currentPage = 1;
+        let sort = {"logindata":-1};
+        let condition = {};
+        let skipNum = (currentPage - 1) * pageSize;
+
+        User.find(condition).skip(skipNum).limit(pageSize).sort(sort).exec((err,res)=>{
+            err?console.log('err',err):console.log('res',res);
+        })
+    }
 }
 
 
--- test_308_mongoose/model/user.js
+++ test_308_mongoose/model/user.js
@@ -5,11 +5,13 @@
 const mongoose = require("./db.js");
 let Schema = mongoose.Schema;
 
+//给Schema建立索引   index
+//给Schema建立默认值 default
 let UserSchema = new Schema({
-    username:{type:String},
+    username:{type:String ,index:true},
     age:{type:String},
     pwd:{type:String},
-    link:{type:String},
+    link:{type:String,default:Date.now},
 })
 
 module.exports = mongoose.model("User",UserSchema);
