19010c5b79de09c1f003bc3e39a1057ee35cf51c
LinearLaw <m18720991459_1@163.com>
2018-01-22 22:35:53 +0800

    Fix bug of login

--- test_309_login/models/db.js
+++ test_309_login/models/db.js
@@ -18,7 +18,7 @@ var url = "http://127.0.0.1:27107";
  */
     init();
     function init(){
-        _connectDb((err,db)=>{
+        _connectDB((err,db)=>{
             if(err){
                 console.log("db error");
                 return;
