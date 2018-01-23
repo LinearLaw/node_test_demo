051378be356fcb6260bc9a4d6c049acbcca92a94
LinearLaw <m18720991459_1@163.com>
2018-01-23 23:32:40 +0800

    Fix bug of mongodb callback

--- test_309_login/models/db.js
+++ test_309_login/models/db.js
@@ -59,7 +59,7 @@ exports.insertOne = (collectionName,json,callback)=>{
 
 /**
  * @desc 查找一条数据
- */
+ *//*
 exports.find = function(collectionName,json,C,D){
     var result = [];
     var callback = null;
@@ -84,7 +84,6 @@ exports.find = function(collectionName,json,C,D){
     _connectDB((err,db)=>{
         var cursor = db.db("linear").collection(collectionName).find(json).skip(skipnumber).limit(limit).sort(sort);
         cursor.each((err,doc)=>{
-            
             if(err){
                 callback(err,null);
                 db.close();
@@ -99,4 +98,48 @@ exports.find = function(collectionName,json,C,D){
             }
         })
     })
+}
+*/
+
+//查找数据，找到所有数据。args是个对象{"pageamount":10,"page":10}
+exports.find = function (collectionName, json, C, D) {
+    var result = [];    //结果数组
+    if (arguments.length == 3) {
+        //那么参数C就是callback，参数D没有传。
+        var callback = C;
+        var skipnumber = 0;
+        //数目限制
+        var limit = 0;
+    } else if (arguments.length == 4) {
+        var callback = D;
+        var args = C;
+        //应该省略的条数
+        var skipnumber = args.pageamount * args.page || 0;
+        //数目限制
+        var limit = args.pageamount || 0;
+        //排序方式
+        var sort = args.sort || {};
+    } else {
+        throw new Error("find函数的参数个数，必须是3个，或者4个。");
+        return;
+    }
+
+    //连接数据库，连接之后查找所有
+    _connectDB(function (err, db) {
+        var cursor = db.db("linear").collection(collectionName).find(json).skip(skipnumber).limit(limit).sort(sort);
+        cursor.each(function (err, doc) {
+            if (err) {
+                callback(err, null);
+                db.close(); //关闭数据库
+                return;
+            }
+            if (doc != null) {
+                result.push(doc);   //放入结果数组
+            } else {
+                //遍历结束，没有更多的文档了
+                callback(null, result);
+                db.close(); //关闭数据库
+            }
+        });
+    });
 }
