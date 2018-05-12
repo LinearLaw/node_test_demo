f32ff7b3c31590ff723b1041a809f6ca3df8bb83
LinearLaw <m18720991459_1@163.com>
2018-05-12 23:03:25 +0800

    update writefile logic

--- test_312_getHtml/app.js
+++ test_312_getHtml/app.js
@@ -1,13 +1,18 @@
+/**
+ * @desc 利用cheerio和iconv，结合request模块，
+ *      对网站内容进行爬取。
+ */
+
 const express =require("express");
 const app = express();
 const request = require("request");
 
+const fs = require("fs");
 //cheerio，用来进行DOM操作
 const cheerio = require("cheerio");
 
-//转码工具，目标网站为gb2312
-var iconv = require('iconv-lite');
-var fs = require("fs");
+//转码工具，目标网站编码格式为gb2312
+const iconv = require('iconv-lite');
 
 function getHtml(i){
     let url = "http://it.dgzx.net/xszp/4cmgames-own/web/gz/g26ld/dhxch/"+i+".htm"
@@ -15,14 +20,24 @@ function getHtml(i){
         encoding: null,
         url: url
     },(err,res,body)=>{
-        //res.body即为所获得的html页面
+        //res.body或body即为所获得的html页面
         // console.log(res.body);
+
+        //1、使用iconv对body进行转码
         body = iconv.decode(body,'gb2312');
+
+        // 2、cheerio读取页面DOM
         var $ = cheerio.load(body);
 
-        var title = $(".f3").text();
-        var content = $(".f2").text();
-        var result = title + "/n  " + content; 
+        //3、获取指定的文本内容
+        var title = $(".f3").text().replace("<br>","\r\n");
+        var content = "";
+        $(".f2").map(function(index,item){
+            var temp = $(item).text().replace("<br>","\r\n")
+            content = content + temp + "\r\n";
+        });
+
+        var result = title + "\r\n  " + content; 
         fs.writeFile('./computer_'+i+'.txt',result,{flag:'w',encoding:'utf-8',mode:'0666'},function(err){
             if(err){
                 console.log("文件写入失败")
