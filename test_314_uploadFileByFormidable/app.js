3282b515b20059b9f1da9365970a34a328b8d844
LinearLaw <17817363770@163.com>
2018-10-24 09:46:29 +0800

    add notice text

--- test_314_uploadFileByFormidable/app.js
+++ test_314_uploadFileByFormidable/app.js
@@ -24,6 +24,7 @@ app.use(function (req, res, next) {
 
 
 app.post("/upload",(req,res)=>{
+  console.log("接收到了文件，时间："+new Date());
   let form = new formidable.IncomingForm();
   form.parse(req, (err, fields, files) => {
 
